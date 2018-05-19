eb0e5c4110345b5e098153faba49c26d80e550b8
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-05-19 21:41:55 +0900

    写真足してみた

--- src/components/SwipeOnsen.js
+++ src/components/SwipeOnsen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
+  Image,
   View
 } from 'react-native';
 
@@ -33,6 +33,10 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    flex: 1
   }
 })
 
@@ -42,13 +46,13 @@ export default class SwipeOnsen extends Component {
       return (
         <Swiper style={styles.wrapper} showsButtons={true}>
           <View style={styles.slide1}>
-            <Text style={styles.text}>温泉</Text>
+            <Image style={styles.image} source={require('../../img/img1.jpg')}/>
           </View>
           <View style={styles.slide2}>
-            <Text style={styles.text}>に</Text>
+            <Image style={styles.image} source={require('../../img/img2.jpg')}/>
           </View>
           <View style={styles.slide3}>
-            <Text style={styles.text}>行きたーい！！！</Text>
+            <Image style={styles.image} source={require('../../img/img3.jpg')}/>
           </View>
         </Swiper>
       );
