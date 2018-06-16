ff66fc0bfdc37ae53dc2f83ab8b3d7850ecf7139
Rentearo Igari <>
2018-06-16 22:56:50 +0900

    コメント追記

--- utilities/apiClient.js
+++ utilities/apiClient.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+// for suzuri api
 const suzuriClient = axios.create({
   baseURL: 'https://suzuri.jp/api/v1',
   timeout: 1000
 });
 
+// for spotify api
 const spotifyClient = axios.create({
 	baseUrl: 'https://api.spotify.com/v1',
 	timeout: 1000
