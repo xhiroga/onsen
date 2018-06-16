4768101a32fe56352f4e211b1c56859aa67a02d3
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-06-16 23:34:17 +0900

    add endpoint sample

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var param = {
+  "img-art": "https://s3.amazonaws.com/hiroga/onsen-tsgen/plimg.png",
+  "ts-art": "https://s3.amazonaws.com/hiroga/onsen-tsgen/tshirts.png",
+  "img-llc": "https://s3.amazonaws.com/hiroga/onsen-tsgen/plimg.png",
+  "ts-llc": "https://s3.amazonaws.com/hiroga/onsen-tsgen/tshirts.png",
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/tsgen', (req, res) => res.send(param))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
