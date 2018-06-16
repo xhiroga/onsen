cacc5e6e21e26221a4f955ae38f6161fcce5da9e
ikeda <ikd618@gmail.com>
2018-06-16 22:57:46 +0900

    Add base Preview component

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SwipeOnsen from './src/components/SwipeOnsen';
+import { Preview } from './src/pages';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{height: 300, justifyContent: 'center'}}>
-          <Text>温泉に行きたいぞ！！！</Text>
-        </View>
-        <SwipeOnsen/>
+        <Preview/>
       </View>
     );
   }
