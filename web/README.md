ebdf992541b72ec398dac10cd790d721bda43495
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-06-24 11:14:32 +0900

    設定ファイルの調整

--- web/README.md
+++ web/README.md
@@ -1,4 +1,10 @@
 # プレイリストをTシャツにできるWebサイト
+https://s3.amazonaws.com/playlist.sticker.io/index.html
+
+# デプロイ方法
+```sh
+./sspa deploy_bucket playlist.sticker.io
+```
 
 # Reference
 Based on [learnjs](https://github.com/benrady/learnjs)
--- web/public/tests/app_spec.js
+++ web/public/tests/app_spec.js
@@ -1,47 +1,10 @@
 describe('PlaylistStickerは', function () {
-    it('プレイリストのURLを渡されたら画面を処理中に差し替える', function () {
-
-
+    it('プレイリストのURLを渡されたらURLハッシュを変更する', function () {
     })
 
-    it('ステッカーのURLが返ってきたらステッカーを表示する', function () {
-        playlistSticker.showView('#playlist-2X3SX875sosVFp58m8puKv');
-        expect($('.view-container .sticker-view').length).toEqual(1);
+    it('プレイリストのURLハッシュがあればステッカーを取得する', function () {
     })
 
-    it('ハッシュがなければランディングページを表示する', function () {
-        playlistSticker.showView('');
-        expect($('.view-container .landing-view').length).toEqual(1);
+    it('URLハッシュがなければランディングページを表示する', function () {
     });
-
-
-    // it('problemビューを表示できる', function(){
-    //     learnjs.showView('#problem-1');
-    //     expect($('.view-container .problem-view').length).toEqual(1);
-    // });
-
-    // it('ハッシュがなければランディングページを表示する', function(){
-    //     learnjs.showView('');
-    //     expect($('.view-container .landing-view').length).toEqual(1);
-    // });
-
-    // // スパイ
-    // it('ビュー関数にハッシュビューパラメータを渡すこと', function() {
-    //     spyOn(learnjs, 'problemView'); // スパイ対象のオブジェクト(=名前空間), 関数名の文字列
-    //     learnjs.showView('#problem-42');
-    //     expect(learnjs.problemView).toHaveBeenCalledWith('42');
-    // });
-
-    // it('ロードされたらルータを呼び出す', function(){
-    //     spyOn(learnjs, 'showView'); //このテスト中のshowViewの動きを記録しておき、あとで専用の関数でアサートする
-    //     learnjs.appOnReady();
-    //     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
-    // });
-
-    // describe('problem viewは', function(){
-    //     it('problem numberを含んだタイトルを持っている', function() {
-    //         var view = learnjs.problemView('1');
-    //         expect(view.text()).toEqual('Problem #1 Coming soon!');
-    //     });
-    // });
 });
